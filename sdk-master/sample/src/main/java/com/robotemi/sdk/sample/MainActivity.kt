fa50fd85afe8a0f59db444b8c97ee64578d2ab16
Will Habsburg <willhabsburg@gmail.com>
2024-04-12 17:28:50 -0400

    BugFix! Had two WillsGoTo methods

--- sdk-master/sample/src/main/java/com/robotemi/sdk/sample/MainActivity.kt
+++ sdk-master/sample/src/main/java/com/robotemi/sdk/sample/MainActivity.kt
@@ -897,67 +897,7 @@ class MainActivity : AppCompatActivity(), NlpListener, OnRobotReadyListener,
     /**
      * Test functionality of going to an elevator, then into, then out of, then home
      */
-    private fun WillsGoTo() {
-        var myLocations = arrayOf("ea3outpasselev", "ea3inpasselev", "ea3outpasselev", "home base")
-        var myLocation = myLocations[sequenceNum]
-        //error message
-        runOnUiThread {
-            printLog("\nTrying to go to location: $myLocation")
-        }
 
-        for (location in robot.locations) {
-
-            if(myLocation == "ea3outpasselev") {
-                runOnUiThread {
-                    printLog("\n\n working \n\n")
-                    robot.speak(create("---Going to elevator" , false, cached = true))
-                }
-            }
-
-            if(myLocation == "ea3inpasselev") {
-                runOnUiThread {
-                    printLog("\n\n working \n\n")
-                    robot.speak(create("---Please open the elevator" , false, cached = true))
-                }
-            }
-
-            if (location.lowercase() == myLocation.lowercase()
-                    .trim { it <= ' ' }
-            ) {
-
-                robot.goTo(
-                    myLocation.lowercase().trim { it <= ' ' },
-                    backwards = false,
-                    noBypass = false,
-                    speedLevel = SpeedLevel.HIGH
-                )
-
-
-            } else {
-                runOnUiThread {
-                    printLog("\nLocation not found $myLocation")
-                }
-            }
-        }
-        if (myLocation == "home base") {
-            runOnUiThread {
-                printLog("\nYou are home.  I'm going back to the main screen")
-            }
-        }
-
-        else if (myLocation == "ea3inpasselev") {
-            runOnUiThread {
-                robot.speak(create("---Please push the elevator button for me!", false))
-            }
-        }
-        else {
-            var nextLoc = myLocations[sequenceNum + 1]
-            runOnUiThread {
-                printLog("\nPress button again to go to next location: $nextLoc")
-            }
-        }
-        sequenceNum++
-    }
 
     /**
      * Modified version of loadMap function with dialog removed. ID is hardcoded in the onClick handler above.
